d014555fde2a705f9ee2b1d522f2308da060db3d
Erdem Alpay <erdemalpay@gmail.com>
2022-03-28 08:59:17 +0300

    Add an initial unit test with jest

---
+++ __test__/pages/home/[location].test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from "@testing-library/react";
+import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import TablesPage from "../../../pages/home/[location]";
+import { Game, Table, User } from "../../../types";
+
+const wrapper = (props: { children: ReactNode }) => {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {props.children}
+    </QueryClientProvider>
+  );
+};
+
+describe("TablesPage", () => {
+  const initialTables: Table[] = [];
+  const mentors: User[] = [];
+  const location = 1;
+  const initialGames: Game[] = [];
+
+  it("renders a heading", () => {
+    const { getByText } = render(
+      <TablesPage
+        initialGames={initialGames}
+        location={location}
+        initialTables={initialTables}
+        mentors={mentors}
+      />,
+      { wrapper }
+    );
+
+    const heading = getByText(/add table/i);
+  });
+});
