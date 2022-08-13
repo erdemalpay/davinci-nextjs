ddd913a0bb24da96c3702f702207e076adf28eee
gokhankaraoglu <gkaraoglu91@gmail.com>
2022-08-13 17:06:28 +0300

    add pagination at games page

---
+++ components/Pagination.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+export function Pagination({
+  page,
+  startIndex,
+  gameLimitPerPage,
+  gamesCount,
+  totalPages,
+  handleClick,
+}: {
+  page: number;
+  startIndex: number;
+  gameLimitPerPage: number;
+  gamesCount: number;
+  totalPages: number;
+  handleClick: (num: number) => void;
+}) {
+  return (
+    <nav
+      className="flex justify-between items-center p-4"
+      aria-label="Table navigation"
+    >
+      <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
+        Showing{" "}
+        <span className="font-semibold text-gray-900 dark:text-white">
+          {startIndex + 1}-{startIndex + gameLimitPerPage}
+        </span>{" "}
+        of{" "}
+        <span className="font-semibold text-gray-900 dark:text-white">
+          {gamesCount}
+        </span>
+      </span>
+      <ul className="inline-flex items-center -space-x-px">
+        <li>
+          <button
+            className="block py-2 px-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            onClick={() => handleClick(page - 2)}
+          >
+            Previous
+          </button>
+        </li>
+        <li>
+          <button
+            className="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            onClick={() => handleClick(page - 1)}
+          >
+            {page === 1 ? "-" : page - 1}
+          </button>
+        </li>
+        <li>
+          <button
+            aria-current="page"
+            className="z-10 py-2 px-3 leading-tight text-blue-600 bg-blue-50 border border-blue-300 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
+            onClick={() => handleClick(page)}
+          >
+            {page}
+          </button>
+        </li>
+        <li>
+          <button
+            className="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            onClick={() => handleClick(page + 1)}
+          >
+            {page === 31 ? "-" : page + 1}
+          </button>
+        </li>
+        <li>
+          <button
+            className="block py-2 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            onClick={() => handleClick(page + 2)}
+          >
+            Next
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+}
--- pages/games.tsx
+++ pages/games.tsx
@@ -1,10 +1,9 @@
+import { useEffect, useState } from "react";
 import { GetServerSideProps } from "next";
-import Cookies from "js-cookie";
 import type { Game } from "../types";
-import { useQuery } from "react-query";
-import { getGames } from "../utils/api/game";
-
-const path = "/user/profile";
+import { getGames, useGetGames } from "../utils/api/game";
+import { GameListItem } from "../components/GameListItem";
+import { Pagination } from "../components/Pagination";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const user = await getGames({ context });
@@ -12,33 +11,28 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 };
 
 export default function Games({ games: initialGames }: { games: Game[] }) {
-  const token = Cookies.get("jwt");
+  const [filteredGames, setFilteredGames] = useState<Game[]>([]);
+  const [page, setPage] = useState(1);
+  const [gameLimitPerPage, setpageListLimit] = useState(15);
+  const [totalPages, setTotalPages] = useState(0);
+  const startIndex = (page - 1) * gameLimitPerPage;
+  const { isLoading, error, games } = useGetGames(initialGames);
+  const [gamesCount, setGamesCount] = useState(0);
 
-  const { data, error } = useQuery(path, () => getGames(), {
-    initialData: initialGames,
-  });
+  useEffect(() => {
+    if (!games) return;
+    setGamesCount(games?.length);
+    setFilteredGames(games?.slice(startIndex, startIndex + gameLimitPerPage));
+    setTotalPages(Math.ceil(gamesCount / gameLimitPerPage));
+  }, [page, games, gamesCount, gameLimitPerPage, startIndex]);
+
   if (error) return <p>An error has occurred.</p>;
-  if (!data) return <p>Loading...</p>;
-  const games = data || initialGames;
-  const gameList: any = [];
-  games.forEach((game) => {
-    gameList.push(
-      <tr
-        key={game._id}
-        className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
-      >
-        <th
-          scope="row"
-          className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-        >
-          {game.name}
-        </th>
-        <td className="py-4 px-6">{game.expansion}</td>
-        <td className="py-4 px-6">-</td>
-      </tr>
-    );
-  });
-  // const { name, role } = user;
+  if (isLoading) return <p>Loading...</p>;
+
+  const handleClick = (num: number) => {
+    if (num > 0 && num < totalPages + 1) setPage(num);
+  };
+
   return (
     <div className="m-8">
       <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
@@ -56,108 +50,20 @@ export default function Games({ games: initialGames }: { games: Game[] }) {
               </th>
             </tr>
           </thead>
-          <tbody>{gameList}</tbody>
+          <tbody>
+            {filteredGames.map((game) => (
+              <GameListItem game={game} key={game._id} />
+            ))}
+          </tbody>
         </table>
-        <nav
-          className="flex justify-between items-center pt-4"
-          aria-label="Table navigation"
-        >
-          <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
-            Showing{" "}
-            <span className="font-semibold text-gray-900 dark:text-white">
-              1-10
-            </span>{" "}
-            of{" "}
-            <span className="font-semibold text-gray-900 dark:text-white">
-              {games.length}
-            </span>
-          </span>
-          <ul className="inline-flex items-center -space-x-px">
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-              >
-                <span className="sr-only">Previous</span>
-                <svg
-                  className="w-5 h-5"
-                  aria-hidden="true"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-              >
-                1
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-              >
-                2
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                aria-current="page"
-                className="z-10 py-2 px-3 leading-tight text-blue-600 bg-blue-50 border border-blue-300 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
-              >
-                3
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-              >
-                ...
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-              >
-                100
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-              >
-                <span className="sr-only">Next</span>
-                <svg
-                  className="w-5 h-5"
-                  aria-hidden="true"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-              </a>
-            </li>
-          </ul>
-        </nav>
+        <Pagination
+          page={page}
+          startIndex={startIndex}
+          gameLimitPerPage={gameLimitPerPage}
+          gamesCount={gamesCount}
+          totalPages={totalPages}
+          handleClick={handleClick}
+        ></Pagination>
       </div>
     </div>
   );
