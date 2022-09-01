3ffa8f74b57db31d8edf09b20854d5e919d14d82
Erdem Alpay <erdemalpay@gmail.com>
2022-09-01 23:16:01 +0300

    Add visits page

--- components/header/PageSelector.tsx
+++ components/header/PageSelector.tsx
@@ -34,6 +34,10 @@ export function PageSelector() {
       path: "/users",
     },
     {
+      name: "Visits",
+      path: "/visits",
+    },
+    {
       name: "Menu",
       path: "/menu",
     },
--- pages/visits.tsx
+++ pages/visits.tsx
@@ -28,16 +28,34 @@ export default function Analytics() {
       <Header showLocationSelector={false} />
 
       <div className="mx-[20px]">
-        <div className="flex flex-end w-1/2">
-          <label className="flex items-center">Location:</label>
-          <select
-            onChange={(value) => setLocation(Number(value.target.value))}
-            className="py-2border-b-grey-300 focus:outline-none text-sm"
-            value={location}
-          >
-            <option value={1}>Bahçeli</option>
-            <option value={2}>Neorama</option>
-          </select>
+        <div className="flex justıfy-end">
+          <div className="mt-4 flex justify-between w-full">
+            <h1 className="text-4xl">
+              {location === 1 ? "Bahçeli" : "Neorama"}
+            </h1>
+            <div className="flex justify-end w-full">
+              <button
+                onClick={() => setLocation(1)}
+                className={`relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 ${
+                  location === 1 ? "ring-2 outline-none ring-red-800" : ""
+                }`}
+              >
+                <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md">
+                  Bahçeli
+                </span>
+              </button>
+              <button
+                onClick={() => setLocation(2)}
+                className={`relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-red-500 ${
+                  location === 2 ? "ring-2 outline-none ring-red-800" : ""
+                }`}
+              >
+                <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md">
+                  Neorama
+                </span>
+              </button>
+            </div>
+          </div>
         </div>
         <MonthlyCalendar
           currentMonth={currentMonth}
