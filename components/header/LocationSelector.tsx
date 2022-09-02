e6c024e8605ee54d9e53277919930325e0dd1f2b
Erdem Alpay <erdemalpay@gmail.com>
2022-09-02 21:28:47 +0300

    Fix location selector

--- components/header/LocationSelector.tsx
+++ components/header/LocationSelector.tsx
@@ -12,17 +12,21 @@ export function LocationSelector() {
   const selectedLocation = locations?.find(
     (location) => location._id === selectedLocationId
   );
-  return locations.map((location) => (
-    <button
-      key={location._id}
-      onClick={() => {
-        router.push(`/home/${location._id}`);
-      }}
-      className={`text-sm ${
-        selectedLocation?._id === location._id ? "border-2" : "border-0"
-      } rounded p-2 text-white`}
-    >
-      {location.name}
-    </button>
-  ));
+  return (
+    <>
+      {locations.map((location) => (
+        <button
+          key={location._id}
+          onClick={() => {
+            router.push(`/home/${location._id}`);
+          }}
+          className={`text-sm ${
+            selectedLocation?._id === location._id ? "border-2" : "border-0"
+          } rounded p-2 text-white`}
+        >
+          {location.name}
+        </button>
+      ))}
+    </>
+  );
 }
