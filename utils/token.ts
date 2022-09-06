524f72bb85217fa494648d541fffa91826be03dc
Erdem Alpay <erdemalpay@gmail.com>
2022-09-06 18:15:14 +0300

    fix token name

--- utils/token.ts
+++ utils/token.ts
@@ -3,7 +3,7 @@ import Cookies from "js-cookie";
 export function getToken() {
   let isServer = typeof window === "undefined";
   if (isServer) {
-    return process.env.REVALIDATE_TOKEN || "";
+    return process.env.JWT_TOKEN || "";
   } else {
     return Cookies.get("jwt") || localStorage.getItem("jwt");
   }
